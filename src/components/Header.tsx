d92e8871bde3ddf4f52baa6463702205066f5472
Suhail Jamal <119126634+SuhailJamal@users.noreply.github.com>
2025-05-25 15:12:20 +0530

    Implemented the logic to filter products based on category and price in the sidebar menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,21 @@ import { ShoppingCart, Search } from "lucide-react";
 const Header = () => {
   return (
     <>
-      <nav className="flex justify-between items-center bg-blue-900 text-white p-4 px-2 md:px-14">
+      <nav className="flex justify-between items-center bg-blue-900 text-white p-2 md:p-4 px-2 md:px-14">
         <div className="font-bold text-lg md:text-4xl">Logo</div>
         <div className="flex justify-center gap-4 md:gap-10">
-          <div className="flex items-center border border-white rounded-md bg-blue-900 h-12 px-3 w-[180px] md:w-[400px]">
-            <Search className="text-white w-5 h-5 mr-2" />
+          <div className="flex justify-between items-center border border-white rounded-md bg-blue-900 h-10 md:h-12 px-3 w-[150px] md:w-[400px]">
+            <Search className="hidden md:block text-white w-5 h-5 md:mr-2" />
             <input
               type="text"
               placeholder="Search for products..."
-              className="bg-transparent outline-none text-slate-100 placeholder-slate-100 flex-1"
+              className="bg-transparent outline-none text-slate-100 placeholder-slate-100 flex-1 md:text-xl text-sm"
             />
           </div>
 
-          <div className="flex rounded-md bg-blue-950 px-5 py-2 text-white items-center space-x-4">
+          <div className="flex rounded-md bg-blue-950 px-3 md:px-5 py-2 text-white items-center space-x-4">
             <ShoppingCart />
-            <p>Cart</p>
+            <p className="hidden md:block">Cart</p>
           </div>
         </div>
       </nav>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,15 @@
-export default function Sidebar() {
+interface sideBarProps {
+  category: string;
+  setCategory: (value: string) => void;
+  filterPrice: number;
+  setFilterPrice: (value: number) => void;
+}
+export default function Sidebar({
+  category,
+  setCategory,
+  filterPrice,
+  setFilterPrice,
+}: sideBarProps) {
   return (
     <div className="flex flex-col">
       <div className="bg-blue-900 hidden md:block md:w-[250px] rounded-lg p-4 mb-5 text-white ml-5">
@@ -7,30 +18,59 @@ export default function Sidebar() {
           <h3 className="font-medium mb-2">Category</h3>
           <div className="space-y-2 flex flex-col">
             <label>
-              <input type="radio" name="category" /> All
+              <input
+                type="radio"
+                name="category"
+                value="all"
+                checked={category === "all"}
+                onChange={() => setCategory("all")}
+              />{" "}
+              All
             </label>
             <label>
-              <input type="radio" name="category" /> Electronics
+              <input
+                type="radio"
+                name="category"
+                value="electronics"
+                checked={category === "electronics"}
+                onChange={() => setCategory("electronics")}
+              />{" "}
+              Electronics
             </label>
             <label>
-              <input type="radio" name="category" /> Clothing
+              <input
+                type="radio"
+                name="category"
+                value="clothing"
+                checked={category === "clothing"}
+                onChange={() => setCategory("clothing")}
+              />{" "}
+              Clothing
             </label>
             <label>
-              <input type="radio" name="category" /> Home
+              <input
+                type="radio"
+                name="category"
+                value="home"
+                checked={category === "home"}
+                onChange={() => setCategory("home")}
+              />{" "}
+              Home
             </label>
           </div>
-          <h3 className="mt-4 font-medium mb-2">Price</h3>
           <input
             type="range"
             min={0}
             max={1000}
+            value={filterPrice}
+            onChange={(e) => setFilterPrice(Number(e.target.value))}
             className="w-full accent-white"
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <p>0</p>
+          />{" "}
+          <div className="flex justify-between items-center">
+            <p>0</p>
 
-          <p>1000</p>
+            <p>1000</p>
+          </div>
         </div>
       </div>{" "}
       <div className="bg-white hidden md:block md:w-[250px] rounded-lg p-4 text-black ml-5">
@@ -39,22 +79,54 @@ export default function Sidebar() {
           <h3 className="font-medium mb-2">Category</h3>
           <div className="space-y-2 flex flex-col">
             <label>
-              <input type="radio" name="category" /> All
+              <input
+                type="radio"
+                name="category"
+                value="all"
+                checked={category === "all"}
+                onChange={() => setCategory("all")}
+              />{" "}
+              All
             </label>
             <label>
-              <input type="radio" name="category" /> Electronics
+              <input
+                type="radio"
+                name="category"
+                value="electronics"
+                checked={category === "electronics"}
+                onChange={() => setCategory("electronics")}
+              />{" "}
+              Electronics
             </label>
             <label>
-              <input type="radio" name="category" /> Clothing
+              <input
+                type="radio"
+                name="category"
+                value="clothing"
+                checked={category === "clothing"}
+                onChange={() => setCategory("clothing")}
+              />{" "}
+              Clothing
             </label>
             <label>
-              <input type="radio" name="category" /> Home
+              <input
+                type="radio"
+                name="category"
+                value="home"
+                checked={category === "home"}
+                onChange={() => setCategory("home")}
+              />{" "}
+              Home
             </label>
           </div>
           <h3 className="mt-4 font-bold mb-2">Price</h3>
           <input
             type="number"
             name="price"
+            min="0"
+            max="1000"
+            value={filterPrice}
+            onChange={() => setFilterPrice(Number(filterPrice))}
             className="p-1 rounded-md outline-none border border-black "
           />
         </div>
