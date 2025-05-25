317b4f393553130a1abaf859d687d1ab7393b1de
Suhail Jamal <119126634+SuhailJamal@users.noreply.github.com>
2025-05-25 17:28:36 +0530

    Changed the position of the search now it is in the center and added a user icon in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ShoppingCart, Search } from "lucide-react";
+import { ShoppingCart, Search, User } from "lucide-react";
 import { useState } from "react";
 import SearchPopUp from "./SearchPopUp";
 import Link from "next/link";
@@ -12,24 +12,24 @@ const Header = () => {
         <Link href="/">
           <div className="font-bold text-lg md:text-4xl">Logo</div>
         </Link>
-        <div className="flex justify-center gap-4 md:gap-10">
-          <div className="flex justify-between items-center border border-white rounded-md bg-blue-900 h-10 md:h-12 px-3 w-[150px] md:w-[400px]">
-            <Search className="hidden md:block text-white w-5 h-5 md:mr-2" />
-            <input
-              type="text"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder="Search for products..."
-              className="bg-transparent outline-none text-white placeholder-slate-100 flex-1 md:text-xl text-sm"
-            />
-          </div>
-
+        <div className="flex justify-between items-center border border-white rounded-md bg-blue-900 h-10 md:h-12 px-3 w-[150px] md:w-[400px]">
+          <Search className="hidden md:block text-white w-5 h-5 md:mr-2" />
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search for products..."
+            className="bg-transparent outline-none text-white placeholder-slate-100 flex-1 md:text-xl text-sm"
+          />
+        </div>
+        <div className="flex justify-center gap-2 items-center">
           <Link href="/cart">
             <div className="flex rounded-md bg-blue-950 px-3 md:px-5 py-3 text-white items-center space-x-4">
               <ShoppingCart />
               <p className="hidden md:block">Cart</p>
             </div>
           </Link>
+          <User />
         </div>
       </nav>
     </>
